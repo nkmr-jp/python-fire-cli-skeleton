904a8d5e108323879a81aee2fa76fa9c7d99257d
nkmr-jp <nkmr-jp@users.noreply.github.com>
2018-08-29 08:47:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sample-command",
+    name="sample_command",
     version="0.0.1",
     author="[author]",
     author_email="[author_email]",
